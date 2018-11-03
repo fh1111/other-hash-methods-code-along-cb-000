224fc4a2517ccfbbeb8a2b657afee1f7cea53cba
Fer Hernandez <fh.1111@gmail.com>
2018-11-03 19:00:36 +0000

    automatically backed up by learn

--- lib/min.rb
+++ lib/min.rb
@@ -12,3 +12,10 @@ def get_the_min(groceries)
   #code your solution here!
   puts groceries.min
 end
+
+get_the_min(    {
+      dairy: ["milk", "yogurt", "cheese"],
+      vegetable: ["carrots", "broccoli", "cucumbers"],
+      meat: ["chicken", "steak", "salmon"],
+      grains: ["rice", "pasta"]
+    })
